7c1925061427eb84abeb1e43a9d20e26ea910c14
Pedro Arthur <pedro.art.vel@gmail.com>
2020-02-18 18:53:17 -0300

    Finished

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,45 @@
 class TasksController < ApplicationController
+  before_action :find_task, only: [:show, :edit, :update, :destroy]
   def index
     @tasks = Task.all
+  end
+
+  def show
+    # @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(tasks_params)
+    @task.save
+    redirect_to tasks_path(@tasks)
+  end
+
+  def edit
+    # @task = Task.find(params[:id])
+  end
+
+  def update
+    # @task = Task.find(params[:id])
+    @task.update(tasks_params)
+    redirect_to tasks_path(@tasks)
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path(@tasks)
+  end
+
+  private
+
+  def find_task
+    @task = Task.find(params[:id])
+  end
+
+  def tasks_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
